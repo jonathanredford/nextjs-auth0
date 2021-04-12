43d90fd1445c130d21e7050872a72d99135fdb8c
Jonathan Redford <jonathantredford@gmail.com>
2021-04-13 08:12:08 +1000

    lookup endpoint returns cookie if x-real-ip matches

--- pages/api/ipdata/lookup.js
+++ pages/api/ipdata/lookup.js
@@ -4,24 +4,37 @@ const ipdata = new IPData(process.env.IPDATA_API_KEY)
 
 export default async (req, res) => {
     // res.setHeader('Cache-Control', 'public, s-maxage=71960, stale-while-revalidate=86400'); // 86400 = 1 day
-    console.log(JSON.stringify(req.headers,null,2))
     const { query } = req
     let { ip=null, field=null, fields='ip,country_code,currency'} = query
     if(!ip) {
-        ip = req.headers['x-forwarded-for']
+        ip = req.headers['x-real-ip']
     }
-    if(fields) fields = fields.split(',')
-    const result = await new Promise(resolve => {
-        ipdata.lookup(ip, field, fields)
-        .then(info => {
-            delete info.status
-            resolve(info)
-        })
-        .catch(err => {
-            console.log('Error looking up ip address: ', err)
-            resolve({})
-        })
-    })
 
-    return res.json(result)
+    let cookie
+    try {
+        cookie = JSON.parse(req.cookies.proxyData)
+        console.log(`proxyData cookie found: `, JSON.stringify(cookie, null, 2))
+    } catch(err) {
+        console.log(`No proxyData cookie found. Looking up ${ip}`)
+    }
+
+    if(cookie && cookie.ip === ip) {
+        console.log('cookie ip matches ' + ip)
+        return res.json(cookie)
+    } else {
+        console.log('cookie ip does not match ' + ip)
+        if(fields) fields = fields.split(',')
+        const result = await new Promise(resolve => {
+            ipdata.lookup(ip, field, fields)
+            .then(info => {
+                delete info.status
+                resolve(info)
+            })
+            .catch(err => {
+                console.log('Error looking up ip address: ', err)
+                resolve({})
+            })
+        })
+        return res.json(result)
+    }
 }
