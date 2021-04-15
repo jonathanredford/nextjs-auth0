5e9e21efd41ab7ca23ea1d3a58b3d4ba4f7a00f8
Jonathan Redford <jonathantredford@gmail.com>
2021-04-15 14:48:58 +1000

    added free button to price buttons

--- components/ContentPage.js
+++ components/ContentPage.js
@@ -83,9 +83,31 @@ export default ContentPage;
 
 const PricingButtons = ({contentId, prices}) => {
     const [ proxy ] = useContext(ProxyContext)
-    console.log(proxy)
+
+    return (
+        <Fragment>
+            {
+                prices?.free && <PriceButton contentId={contentId} text={`Free - Add to Library`} />
+            }
+            {
+                prices?.rent && <PriceButton contentId={contentId} text={`Rent ${proxy?.currency?.native}${prices.rent.amount.toFixed(2)}`} />
+            }
+            {
+                prices?.buy && <PriceButton contentId={contentId} className="ml-2" text={`Buy ${proxy?.currency?.native}${prices.buy.amount.toFixed(2)}`} />
+            }
+            {
+                prices?.plan && <PriceButton contentId={contentId} className="ml-2 bg-red-700 text-white hover:bg-gray-100 hover:text-red-700" text={`Subscribe ${proxy?.currency?.native}${prices.plan.price.amount.toFixed(2)}`} />
+            }
+        </Fragment>
+    )
+}
+
+const PriceButton = ({text, contentId, className}) => {
+    const [ isCheckingOut, setIsCheckingOut ] = useState(false)
     const handleCheckout = async (type) => {
         var stripe = Stripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
+
+        setIsCheckingOut(true)
 
         fetch(`/api/checkout?id=${contentId}&type=${type}&callback_url=${window.location.origin}${window.location.pathname}`)
         .then(res => res.json())
@@ -93,28 +115,40 @@ const PricingButtons = ({contentId, prices}) => {
             console.log(session)
             stripe.redirectToCheckout({ sessionId: session.id })
         })
+        .catch(err => {
+            console.log(err)
+            setIsCheckingOut(false)
+        })
     }
 
     return (
-        <Fragment>
+        <button
+            disabled={isCheckingOut ? true : false}
+            onClick={() => handleCheckout('rent')}
+            className={`
+                inline-flex
+                items-center
+                px-4
+                py-2
+                bg-white
+                text-sm
+                font-medium
+                rounded 
+                hover:text-red-700
+                focus:outline-none
+                transition
+                ease-in-out
+                duration-150
+                ${isCheckingOut ? 'cursor-not-allowed' : null}
+                ${className}
+            `}>
             {
-                prices?.rent &&
-                <button onClick={() => handleCheckout('rent')} className="px-8 py-2 bg-white text-gray-900 text-sm font-medium rounded  hover:text-red-700 focus:outline-none">
-                    Rent {proxy?.currency?.native}{prices.rent.amount.toFixed(2)}
-                </button>
+                isCheckingOut && <svg className={`animate-spin -ml-1 mr-3 h-5 w-5 text-gray-900`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                </svg>
             }
-            {
-                prices?.buy &&
-                <button onClick={() => handleCheckout('buy')} className="ml-2 px-8 py-2 bg-white text-gray-900 text-sm font-medium rounded  hover:text-red-700 focus:outline-none">
-                    Buy {proxy?.currency?.native}{prices.buy.amount.toFixed(2)}
-                </button>
-            }
-            {
-                prices?.plan &&
-                <button onClick={() => handleCheckout('subscribe')} className="ml-2 px-8 py-2 bg-red-700 text-white text-sm font-medium rounded hover:bg-gray-100 hover:text-red-700 focus:outline-none">
-                    Subscribe {proxy?.currency?.native}{prices.plan.price.amount.toFixed(2)}
-                </button>
-            }
-        </Fragment>
+            {text}
+        </button>
     )
 }
