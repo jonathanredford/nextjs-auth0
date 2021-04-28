6ff3099ac959ff996c79a1184195600795139bbe
Jonathan Redford <jonathantredford@gmail.com>
2021-04-28 16:27:23 +1000

    updated index page

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,41 @@
 import Error from "next/error";
 import { useRouter } from "next/router";
 import { getClient, usePreviewSubscription } from "../utils/sanity";
-import ProductsPage from "../components/ProductsPage";
+import ContentsPage from "../components/ContentsPage";
 
 const query = `//groq
-  *[_type == "product" && defined(slug.current)]
+  *[_type == "content" && defined(slug.current)]
 `;
 
-function IndexPage(props) {
-  const { productsData, preview } = props;
+function ContentsPageContainer({ contentsData, preview }) {
   const router = useRouter();
-
-  if (!router.isFallback && !productsData) {
+  if (!router.isFallback && !contentsData) {
     return <Error statusCode={404} />;
   }
-  const { data: products } = usePreviewSubscription(query, {
-    initialData: productsData,
+  const { data: contents } = usePreviewSubscription(query, {
+    initialData: contentsData,
     enabled: preview || router.query.preview !== null,
   });
 
-  return (
-    <div className="my-8">
-      <div className="mt-4">
-        <ProductsPage products={products} />
-      </div>
-    </div>
-  );
+  return <ContentsPage contents={contents} />;
 }
 
-export async function getServerSideProps({ params = {}, preview = false }) {
-  const productsData = await getClient(preview).fetch(query);
+export async function getStaticProps({ params = {}, preview = false }) {
+  const contentsData = await getClient(preview).fetch(query);
 
   return {
-    props: {
-      preview,
-      productsData,
-    },
+    props: { preview, contentsData },
+    revalidate: 120
   };
 }
 
-export default IndexPage;
+// export async function getServerSideProps({ params = {}, req, res, preview = false }) {
+//   res.setHeader('Cache-Control', 'public, s-maxage=120, stale-while-revalidate=600'); // 600 = 10 minutes
+//   const contentsData = await getClient(preview).fetch(query);
+
+//   return {
+//     props: { preview, contentsData },
+//   };
+// }
+
+export default ContentsPageContainer;
